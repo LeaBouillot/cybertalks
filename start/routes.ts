48eaf8d2dfafac8eb7ff0d976f0eab5aeb2a97e1
bestdevelope <leabouillot7@gmail.com>
2024-07-02 15:05:34 +0200

    creer les fichier edge

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,16 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.on('/categories').render('pages/categories')
+
+router.on('/categories/:id').render('pages/category')
+
+router.on('/event/:id').render('pages/event')
+
+router.on('/contact').render('pages/contact')
+
+
+
+
+
